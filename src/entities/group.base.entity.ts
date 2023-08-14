66fbde33c87600cb3d33657ec2055c4f2376e383
Yaasiin AYEVA <ayevayaasiin@gmail.com>
2023-08-14 02:57:25 +0000

    Fix : Types

--- src/entities/group.base.entity.ts
+++ src/entities/group.base.entity.ts
@@ -3,15 +3,15 @@ import BaseModel from './base.entity';
 
 export default abstract class DataEntity extends BaseModel {
 
-    @Column({ name: 'one_24' })
+    @Column({ name: 'one_24', type: 'decimal', precision: 10, scale: 1 })
     @Index()
     one24: number;
 
-    @Column({ name: 'two_24' })
+    @Column({ name: 'two_24', type: 'decimal', precision: 10, scale: 1 })
     @Index()
     two24: number;
 
-    @Column({ name: 'Curr_3' })
+    @Column({ name: 'Curr_3', type: 'decimal', precision: 10, scale: 1 })
     @Index()
     curr3: number;
 
--- src/entities/groupA.entiy.ts
+++ src/entities/groupA.entiy.ts
@@ -3,22 +3,5 @@ import DataEntity from './group.base.entity';
 
 @Entity()
 export class GroupA extends DataEntity {
-    // @PrimaryGeneratedColumn()
-    // id: number;
-
-    // @Column({ name: 'one_24' })
-    // @Index()
-    // one24: number;
-
-    // @Column({ name: 'two_24' })
-    // @Index()
-    // two24: number;
-
-    // @Column({ name: 'Curr_3' })
-    // @Index()
-    // curr3: number;
-
-    // @Column({ name: 'Target' })
-    // @Index()
-    // target: number;
+    
 }
